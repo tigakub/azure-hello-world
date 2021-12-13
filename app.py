3abc7baab01541cbef821f96972195407d0c5e24
Edward Janne <tigakub@mac.com>
2021-12-13 15:27:02 -0800

    So how does Flask decide what function to call?

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def app_main():
-	var result = "<html><head><title>Hello World, Python Test App</title</head>\n<body><h1>Hello, World!</h1><p>From python via Azure.</p></body></html>\n"
+def hello():
+	var result = "<html><head><title>Hello World, Python Test App</title></head>\\n<body><h1>Hello, World!</h1><p>From python via Azure.</p></body></html>\\n"
 	return result
 
