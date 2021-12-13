c9947a60793b0f54f416a12961b3a583892d50f5
Edward Janne <tigakub@mac.com>
2021-12-13 15:45:40 -0800

    Grrrrrrrrrrr

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-  return "<html><head><title>Hello World, Python Test App</title></head><body><h1>Hello, World!</h1><p>From python via Azure.</p></body></html>"
-
+  return "Hello, World! From python via Azure."
