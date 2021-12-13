cb817e58a6a416812d64ce85803eef3717527cac
tigakub <tigakub@mac.com>
2021-12-13 15:55:56 -0800

    Update app.py
    ashkldlkhjasdlkjhasdakjlsd

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-  return "Hello, World!"
+  return "<html><head><title>Azure Python Web App</title></head><body><h1>Hello, World!</h1></body></html>"
