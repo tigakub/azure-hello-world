eabe015b8b4ea17f9463a3481959817b6e75b014
tigakub <tigakub@mac.com>
2021-12-13 19:01:15 -0800

    Update app.py
    asdfkjhijh alskdfjh

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello():
   if request.is_json:
     data = request.get_json()
     v1 = data['voltage_1']
-    return f"Voltage 1 = {v1}"
+    return v1
   return 'Fucking unreliable piece of shit'
