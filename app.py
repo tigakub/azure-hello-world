77cfdfdab35e3d5add722520e1208d419285924d
tigakub <tigakub@mac.com>
2021-12-13 18:14:36 -0800

    Update app.py
    aslkdjfhalkjhkasjdfs

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def hello():
   if request.is_json:
     payload = request.json
-    return payload['voltage_1']
+    v1 = payload.get('voltage_1')
+    if v1 == None:
+      return 'Failed to retrieve voltage_1'
+    return v1
   return 'Fucking unreliable piece of shit'
