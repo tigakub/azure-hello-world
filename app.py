c1b1de819d17d95f8dcd3a5823730cdc22bec25b
tigakub <tigakub@mac.com>
2021-12-13 19:56:48 -0800

    Update app.py
    daslkfjhljhg asdpiuh lkjabhsdf

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def hello():
   if request.is_json:
     dataDict = request.get_json()
-    req = requests.post('https://thingsboard.cloud/api/v1/Vxf3gbLjb0jhFtyz4kXJ/telemetry', data=dataDict)
+    req = requests.post('https://thingsboard.cloud/api/v1/Vxf3gbLjb0jhFtyz4kXJ/telemetry', json=dataDict)
     return 'Success'
   return 'Failed to extract payload'
