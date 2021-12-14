bd5850f53f9c4724430e9f21432398a7c9018c20
tigakub <tigakub@mac.com>
2021-12-13 19:02:16 -0800

    Update app.py
    lkajsdhflkjhasdf

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import requests
 app = Flask(__name__)
 @app.route('/postjson', methods=['POST'])
 
@@ -7,5 +8,5 @@ def hello():
   if request.is_json:
     data = request.get_json()
     v1 = data['voltage_1']
-    return v1
+    return f"Voltage 1 = {v1}"
   return 'Fucking unreliable piece of shit'
