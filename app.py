6dfef5ff4156d801687fd3ab5db214a0abb20959
tigakub <tigakub@mac.com>
2021-12-13 16:33:10 -0800

    Update app.py
    Testing JSON

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route("/")
+from flask import request
+import json
 
-def hello():
-  return "<html><head><title>Azure Python Web App</title></head><body><h1>Hello, World!</h1></body></html>"
+app = Flask(__name__)
+@app.route('/postjson', methods=['POST'])
+
+def postJsonHandler():
+  if request.is_json:
+    post_data = request.get_json()
+    return 'JSON posted'
+
