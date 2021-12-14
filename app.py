a39368dd10c2f8b6e3263080e607ef75d109beab
tigakub <tigakub@mac.com>
2021-12-13 16:57:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import request
 import json
+import requests
+
+thingsboard_url = "https://thingsboard.cloud/api/v1/1nVPRLsj7bhDTGKLOidD/telemetry"
 
 app = Flask(__name__)
 @app.route('/postjson', methods=['POST'])
@@ -8,5 +11,5 @@ app = Flask(__name__)
 def postJsonHandler():
   if request.is_json:
     post_data = request.get_json()
-    return 'JSON posted'
-
+    repost = requests.post(thingsboard_url, json=post_data)
+    return repost.status_code
