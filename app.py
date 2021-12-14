cd48527884e3e488a18be5b794b4eccc153a0308
tigakub <tigakub@mac.com>
2021-12-13 18:02:16 -0800

    Update app.py
    adslkjhaskldjfhaklsdfh

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 @app.route('/postjson', methods=['POST'])
 
 def hello():
+  if request.is_json:
+    payload = request.get_json()
+    return payload['voltage_1']
   return 'Fucking unreliable piece of shit'
